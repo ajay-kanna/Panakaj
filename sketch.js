ee7da73bc6c16cd584d21319b5bd56aa432e0e82
Ajay 00007 <68370121+ajay-kanna@users.noreply.github.com>
2021-05-25 10:18:10 +0530

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,18 @@
+class Form {
+
+  constructor() {
+    
+    this.title = createElement('h1');
+  }
+  hide(){
+   
+  }
+
+  display(){
+    this.title.html("Car Racing Game");
+    this.title.position(window.innerWidth/2 - 50, 0);
+
+    
+
+  }
+}
